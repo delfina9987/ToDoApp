a40362136af259dfa03c0ca85b42bd7300fb29ef
Delfina <delfina9987@gmail.com>
2022-06-11 15:00:54 +0200

    create task row and update

---
+++ ToDoApp/Rows/TaskRow.swift
@@ -0,0 +1,27 @@
+//
+//  TaskRow.swift
+//  ToDoApp
+//
+//  Created by Delfina on 11/06/2022.
+//
+
+import SwiftUI
+
+struct TaskRow: View {
+    var task: String
+    var completed: Bool
+    
+    var body: some View {
+        HStack(spacing: 20) {
+            Image(systemName: completed ? "checkmark.circle" : "circle")
+            
+            Text(task)
+        }
+    }
+}
+
+struct TaskRow_Previews: PreviewProvider {
+    static var previews: some View {
+        TaskRow(task: "Make swift project", completed: true)
+    }
+}
