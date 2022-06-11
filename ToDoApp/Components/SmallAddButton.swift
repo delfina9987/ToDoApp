b6e09d292ba3e1bc0b5537396c30b5d41b8ca58c
Delfina <delfina9987@gmail.com>
2022-06-11 15:29:50 +0200

    create and update smalladdbutton

---
+++ ToDoApp/Components/SmallAddButton.swift
@@ -0,0 +1,30 @@
+//
+//  SmallAddButton.swift
+//  ToDoApp
+//
+//  Created by Delfina on 11/06/2022.
+//
+
+import SwiftUI
+
+struct SmallAddButton: View {
+    var body: some View {
+        ZStack {
+            Circle()
+                .frame(width: 50)
+                .foregroundColor(Color(hue: 0.083, saturation: 0.658, brightness: 0.875))
+            
+            Text("+")
+                .font(.title)
+                .fontWeight(.heavy)
+                .foregroundColor(.white)
+        }
+        .frame(height: 50)
+    }
+}
+
+struct SmallAddButton_Previews: PreviewProvider {
+    static var previews: some View {
+        SmallAddButton()
+    }
+}
